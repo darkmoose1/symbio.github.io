773adf49e45a96d4a1e2689de30088bf114a51b8
peter <stevenmussett@gmail.com>
2024-11-29 17:39:43 +1300

    Initial commit

---
+++ startbootstrap-agency-gh-pages/js/script.js
@@ -0,0 +1,83 @@
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Navbar shrink function
+    var navbarShrink = function () {
+        const navbarCollapsible = document.body.querySelector('#mainNav');
+        if (!navbarCollapsible) {
+            return;
+        }
+        if (window.scrollY === 0) {
+            navbarCollapsible.classList.remove('navbar-shrink')
+        } else {
+            navbarCollapsible.classList.add('navbar-shrink')
+        }
+
+    };
+
+    // Shrink the navbar 
+    navbarShrink();
+
+    // Shrink the navbar when page is scrolled
+    document.addEventListener('scroll', navbarShrink);
+
+    //  Activate Bootstrap scrollspy on the main nav element
+    
+    /*
+    const mainNav = document.body.querySelector('#mainNav');
+    if (mainNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#mainNav',
+            rootMargin: '0px 0px -40%',
+        });
+    };
+    */
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+    function setSectionVisibility(home, about, services, contact) {
+        document.getElementById('homeSection').style.display = home ? "block" : "none";
+        document.getElementById('aboutSection').style.display = about ? "block" : "none";
+        document.getElementById('servicesSection').style.display = services ? "block" : "none";
+        document.getElementById('contactSection').style.display = contact ? "block" : "none";
+    };
+
+    document.getElementById("home").onclick = function () {
+        setSectionVisibility(!0, !1, !1, !1);
+    };
+
+    document.getElementById("about").onclick = function () {
+        setSectionVisibility(!1, !0, !1, !1);
+    };
+
+    document.getElementById("services").onclick = function () {
+        setSectionVisibility(!1, !1, !0, !1);
+    };
+
+    document.getElementById("contact").onclick = function () {
+        setSectionVisibility(!1, !1, !1, !0);
+    };
+
+    /*
+    showSmallNav = function (show)
+    {
+        document.getElementById("navbarResponsive").style['display'] = 'block';
+    };
+    */
+    
+    showSmallNav = function (show)
+    {
+        document.getElementById("navbarResponsive").style['display']  = show ? 'block' : 'none';
+    };
+    
+});
