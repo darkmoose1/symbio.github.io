ab25029e09c231a468432053eec327f1b383e5f4
peter <stevenmussett@gmail.com>
2024-12-08 10:23:37 +1300

    Contact section

--- Template Website/js/script.js
+++ Template Website/js/script.js
@@ -12,16 +12,15 @@ window.addEventListener('DOMContentLoaded', event => {
     };
 
     
-    function setSectionVisibility(home, about, services, contact, resource) {
+    function setSectionVisibility(home, about, services, contact, resource, book) {
         document.getElementById('homeSection').style.display = home ? "flex" : "none";
         document.getElementById('aboutSection').style.display = about ? "flex" : "none";
         document.getElementById('servicesSection').style.display = services ? "flex" : "none";
         document.getElementById('contactSection').style.display = contact ? "flex" : "none";
         document.getElementById('resourceSection').style.display = resource ? "flex" : "none";
-
+        document.getElementById('bookSection').style.display = book ? "flex" : "none";
         //scrollToTop();
     };
-
 
     /* Main menu */
     document.getElementById("home-menu-item").onclick = function () {
@@ -29,19 +28,23 @@ window.addEventListener('DOMContentLoaded', event => {
     };
 
     document.getElementById("about-menu-item").onclick = function () {
-        setSectionVisibility(!1, !0, !1, !1, !1);
+        setSectionVisibility(!1, !0, !1, !1, !1, !1);
     };
 
     document.getElementById("services-menu-item").onclick = function () {
-        setSectionVisibility(!1, !1, !0, !1, !1);
+        setSectionVisibility(!1, !1, !0, !1, !1, !1);
     };
 
     document.getElementById("contact-menu-item").onclick = function () {
-        setSectionVisibility(!1, !1, !1, !0, !1);
+        setSectionVisibility(!1, !1, !1, !0, !1, !1);
     };
 
     document.getElementById("resources-menu-item").onclick = function () {
-        setSectionVisibility(!1, !1, !1, !1, !0);
+        setSectionVisibility(!1, !1, !1, !1, !0, !1);
+    };
+
+    document.getElementById("book-menu-item").onclick = function () {
+        setSectionVisibility(!1, !1, !1, !1, !1, !0);
     };
 
     /* Small menu */
@@ -50,20 +53,44 @@ window.addEventListener('DOMContentLoaded', event => {
     };
 
     document.getElementById("about-menu-item-sm").onclick = function () {
-        setSectionVisibility(!1, !0, !1, !1, !1);
+        setSectionVisibility(!1, !0, !1, !1, !1, !1);
     };
 
     document.getElementById("services-menu-item-sm").onclick = function () {
-        setSectionVisibility(!1, !1, !0, !1, !1);
+        setSectionVisibility(!1, !1, !0, !1, !1, !1);
     };
 
     document.getElementById("contact-menu-item-sm").onclick = function () {
-        setSectionVisibility(!1, !1, !1, !0, !1);
+        setSectionVisibility(!1, !1, !1, !0, !1, !1);
     };
 
     document.getElementById("resources-menu-item-sm").onclick = function () {
-        setSectionVisibility(!1, !1, !1, !1, !0);
+        setSectionVisibility(!1, !1, !1, !1, !0, !1);
     };
 
+    document.getElementById("book-menu-item-sm").onclick = function () {
+        setSectionVisibility(!1, !1, !1, !1, !1, !0);
+    };
 
+    var acc = document.getElementsByClassName("accordion");
+    var i;
+
+    for (i = 0; i < acc.length; i++) {
+        
+        acc[i].addEventListener("click", function() {
+        
+            /* Toggle between adding and removing the "active" class,
+            to highlight the button that controls the panel */
+            this.classList.toggle("active");
+
+            /* Toggle between hiding and showing the active panel */
+            var panel = this.nextElementSibling;
+
+            if (panel.style.display === "block") {
+                panel.style.display = "none";
+            } else {
+                panel.style.display = "block";
+            }
+        });
+    }
 });
