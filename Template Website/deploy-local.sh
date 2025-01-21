c5749ee7a79d9ca985a968a5d2081d41bb866b51
Steven Mussett <stevenmussett@gmail.com>
2025-01-21 17:49:01 +1300

    Delete Template Website directory

--- Template Website/deploy-local.sh
+++
@@ -1,1 +0,0 @@
-sudo rsync -havz --exclude '*.sh' "/media/peter/Data/GitRepositories/JennyMussett@github.io/Template Website/" "/var/www/tutorial/"
--- Template Website/minify-files.sh
+++
@@ -1,5 +0,0 @@
-wget --post-data="input=`cat css/styles.css`" --output-document=css/min.css https://www.toptal.com/developers/cssminifier/api/raw
-#wget --post-data="input=`cat index.html`" --output-document=index-min.html https://www.toptal.com/developers/html-minifier/api/raw
-
-# For html minification Go to https://codebeautify.org/minify-html
-# Go to https://codebeautify.org/css-beautify-minify
